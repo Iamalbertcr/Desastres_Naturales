3bc60bd36245f253f942742b9b8cbebd978610c4
AlbertoHidalgoL <hidalgoalberto45@gmail.com>
2026-04-18 23:59:57 -0600

    Modificación 2

--- guardar_opinion.php
+++ guardar_opinion.php
@@ -8,9 +8,15 @@ $opinion = $_POST['opinion'];
 $sql = "INSERT INTO opiniones (nombre, fecha, opinion)
 VALUES ('$nombre', '$fecha', '$opinion')";
 
+echo '<div class="main">';
+
 if ($conn->query($sql)) {
-    echo "Guardado correctamente <a href='ver_opiniones.php'>Ver opiniones</a>";
+    echo "<h2>Opinión guardada correctamente ✅</h2>";
+    echo '<a href="ver_opiniones.php" class="btn-volver">Ver opiniones</a>';
 } else {
-    echo "Error";
+    echo "<h2>Error al guardar ❌</h2>";
 }
+
+echo '<br><br><a href="index.php" class="btn-volver">← Volver al inicio</a>';
+echo '</div>';
 ?>
--- ver_opiniones.php
+++ ver_opiniones.php
@@ -3,12 +3,16 @@ include("conexion.php");
 
 $result = $conn->query("SELECT * FROM opiniones");
 
+echo '<div class="main">';
 echo "<h2>Opiniones</h2>";
 
 while($row = $result->fetch_assoc()){
-    echo "<p><b>".$row['nombre']."</b> (".$row['fecha'].")<br>";
-    echo $row['opinion']."</p><hr>";
+    echo "<p>";
+    echo "<b>".$row['nombre']."</b> (".$row['fecha'].")<br>";
+    echo $row['opinion'];
+    echo "</p><hr>";
 }
-echo '<br><a href="index.php" class="btn-volver">← Volver al inicio</a>';
-?>
+
+echo '<a href="index.php" class="btn-volver">← Volver al inicio</a>';
+echo '</div>';
 ?>
